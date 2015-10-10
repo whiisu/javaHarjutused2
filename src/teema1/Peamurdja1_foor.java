4bf35248be91ff2534b8b969fbb23c57b175077b
Kaia Ernits <kaernits@klass3-18-l.intra.itcollege.ee>
2015-10-10 15:19:33 +0300

    praktikum1

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,6 +1,10 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
+
+import java.applet.Applet;
 
 /**
  * Kasuta objekti Foor, et teha valgusfoori tsükkel 5 korda.
@@ -17,5 +21,57 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application
+{
+    public void start(Stage primaryStage) throws Exception
+    {
+        Foor foor = new Foor();
+        int count = 5;
+
+        for (int i = 0; i < count; i++)
+        {
+            blinkRed(foor);
+            blinkYellow(foor);
+            blinkGreen(foor);
+            blinkYellow1(foor);
+        }
+    }
+
+    private void blinkRed(Foor foor)
+    {
+        foor.vahetaPunast();
+        foor.paus(5);
+    }
+
+    private void blinkYellow(Foor foor)
+    {
+        foor.vahetaKollast();
+        foor.paus(1);
+        foor.vahetaKollast();
+        foor.vahetaPunast();
+    }
+
+    private void blinkYellow1(Foor foor)
+    {
+        foor.vahetaKollast();
+        foor.paus(0.5);
+        foor.vahetaKollast();
+    }
+
+    private void blinkGreen(Foor foor)
+    {
+
+        foor.vahetaRohelist();
+        foor.paus(2);
+        foor.vahetaRohelist();
+
+        int count = 0;
+        while (count<6)
+        {
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+
+            count++;
+        }
+    }
 }
