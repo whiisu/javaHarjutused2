89cf457e734635c2bf572b5b9ae8a856d99435f4
kaernits <whiisu@msn.com>
2015-12-26 14:29:25 +0200

    UserDetails

--- src/SQL_Login/Andmebas.java
+++ src/SQL_Login/Andmebas.java
@@ -1,8 +1,6 @@
 package SQL_Login;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Created by Kaia on 26.12.2015.
@@ -42,6 +40,36 @@ public class Andmebas {
             e.printStackTrace();
         }
     }
-
+    public boolean login(String username, String password) {
+        try{
+            Statement stat = conn.createStatement();
+            // SELECT on nagu excelis hiirega "selekteeriks" mingeid kaste. SQLis tähendab konkreetselt,
+            // et milliste tulpade infot soovid kätte saada. WHERE'ga käsed välja võtta ainult tingimustele vastavad väljad.
+            String sql = "SELECT * FROM USERS WHERE USERNAME = '\" + username + \"' LIMIT 1;";
+            // Kuna tegu on päringuga siis käsuks on executeQuery ja ta tagastab andme objekti ResultSet.
+            ResultSet rs = stat.executeQuery(sql);
+            // Kui Query andmeid ei tagastanud (päring ei toonud tulemusi) siis rs-i kasutada ei saa.
+            // Seepärast, kui kasutajat ei eksisteeri tuleb lihtsalt error ja "return" käsuni ei jõutagi.
+            // Aga jõutakse lõpu "return false" käsuni küll.
+            String dbPassword = rs.getString("password");
+            rs.close();
+            stat.close();
+            return  password.equals(dbPassword);
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+        return false;
+    }
+    // Kui programmis avad ainult ühendusi ja ühtegi ei sulge siis see kulutab arvuti (serveri) ressursse.
+    // Üsna kiiresti võib masina kokku jooksutada.
+    public void sulgeYhendus() {
+        try {
+            conn.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        System.out.println("Ühendus suletud");
+    }
 
 }
---
+++ src/SQL_Login/UserDetails.java
@@ -0,0 +1,69 @@
+package SQL_Login;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+
+/**
+ * Created by Kaia on 26.12.2015.
+ */
+public class UserDetails {
+    private String kasutajanimi;
+    private Stage stage = new Stage();
+    private Button updateButton;
+    private Button logoutButton;
+    private HashMap<String, String> andmed;
+
+    public UserDetails(String kasutajaSisse){
+        kasutajanimi = kasutajaSisse;
+        setupStage();
+        //setupUpdate();
+        //setupLogout();
+    }
+
+    private void setupStage() {
+        TilePane tile = new TilePane();
+        tile.setPrefColumns(2);
+        Scene scene= new Scene(tile);
+        stage.setScene(scene);
+
+        Andmebas a = new Andmebas();
+        andmed = a.getUser(kasutajanimi);
+
+        TextField kasutajanimiField = new TextField(andmed.get("username"));
+        PasswordField paroolField = new PasswordField();
+        paroolField.setText(andmed.get("password"));
+        TextField fullnameField = new TextField(andmed.get("fullname"));
+        TextField numberField = new TextField(andmed.get("number"));
+        TextField aadressField = new TextField(andmed.get("address"));
+        updateButton = new Button("Uuenda andmeid");
+
+        updateButton.setOnAction(event -> {
+            HashMap<String, String> uuedAndmed = new HashMap<String, String>();
+            uuedAndmed.put("username", kasutajanimiField.getText());
+            uuedAndmed.put("password", paroolField.getText());
+            uuedAndmed.put("fullname", fullnameField.getText());
+            uuedAndmed.put("number", numberField.getText());
+            uuedAndmed.put("address", aadressField.getText());
+
+            a.uuendaKasutajaAndmeid(uuedAndmed);
+            a.sulgeYhendus();
+            stage.close();
+            new UserDetails(kasutajanimiField.getText());
+        });
+        Label l2 = new Label("Kasutajanimi");
+        Label l3 = new Label("Parool");
+        Label l4 = new Label("Päris nimi");
+        Label l5 = new Label("Telefoni number");
+        Label l6 = new Label("Aadress");
+        logoutButton = new Button("Logi välja");
+        tile.getChildren().addAll(l2, kasutajanimiField, l3, paroolField, l4, fullnameField, l5, numberField, l6, aadressField, logoutButton, updateButton);
+
+        stage.show();
+    }
+}
